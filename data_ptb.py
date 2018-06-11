ae7a5c2eff022356a2cc36db58c694afbbfdac85
Qingnan Duan <duanqn@users.noreply.github.com>
2018-06-11 16:39:49 +0800

    Import random

--- data_ptb.py
+++ data_ptb.py
@@ -8,6 +8,8 @@ import torch
 import nltk
 from nltk.corpus import ptb
 
+import random
+
 word_tags = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'JJ', 'JJR', 'JJS', 'LS', 'MD', 'NN', 'NNS', 'NNP', 'NNPS', 'PDT',
              'POS', 'PRP', 'PRP$', 'RB', 'RBR', 'RBS', 'RP', 'SYM', 'TO', 'UH', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ',
              'WDT', 'WP', 'WP$', 'WRB']
