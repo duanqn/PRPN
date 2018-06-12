76f9a43baaea4c818f949630d3f75090a02a047d
Qingnan Duan <duanqn@users.noreply.github.com>
2018-06-12 13:18:15 +0800

    Diagnose

--- ParsingNetwork.py
+++ ParsingNetwork.py
@@ -28,57 +28,51 @@ class ParsingNetwork(nn.Module):
                                   nn.Conv1d(nhid, 2, 1, groups=2),
                                   nn.Sigmoid())
         '''
-        self.gate = nn.RNN(ninp, 2, 2, nonlinearity = 'relu', batch_first = True, dropout = dropout, bidirectional = True)
+        self.g_current = nn.RNN(ninp, 1, 2, nonlinearity = 'relu', batch_first = False, dropout = dropout, bidirectional = True)
+        self.g_apxnext = nn.RNN(ninp, 1, 2, nonlinearity = 'relu', batch_first = False, dropout = dropout, bidirectional = True)
 
     def forward(self, emb, parser_state):
         emb_last, cum_gate = parser_state
         ntimestep = emb.size(0)
         print 'ntimestep = ' + str(ntimestep)
-        print 'emb size: ' + str(emb.size())
         bsz = emb.size(1)
 
-        emb_last = torch.cat([emb_last, emb], dim=0)
-        print 'emb_last size: ' + str(emb_last.size())
-        emb = emb_last.transpose(0, 1).transpose(1, 2)  # bsz, ninp, ntimestep + nlookback
         print 'emb size: ' + str(emb.size())
 
-        h0 = Variable(torch.randn(4, bsz, 2))
+        h0 = Variable(torch.randn(4, bsz, 1))
         emb = emb.transpose(1, 2)
 
         if emb.is_cuda:
             h0 = h0.cuda()
 
-        gates, hidden = self.gate(emb, h0)  # bsz, 2, ntimestep
+        g, _hidden = self.g_current(emb, h0)  # bsz, 2, ntimestep
+        g_next, _hidden = self.g_apxnext(emb, h0)
 
-        gates = gates.transpose(1, 2)
-
-        gate = gates[:, 0, :]
-        gate_next = gates[:, 1, :]
-        cum_gate = torch.cat([cum_gate, gate], dim=1)
+        cum_gate = torch.cat([cum_gate, g], dim=1)
         gate_hat = torch.stack([cum_gate[:, i:i + ntimestep] for i in range(self.nslots, 0, -1)],
                                dim=2)  # bsz, ntimestep, nslots
 
         print 'gate_hat size: ' + str(gate_hat.size())
         print 'cum_gate size: ' + str(cum_gate.size())
-        print 'gate size: ' + str(gate.size())
+        print 'g size: ' + str(g.size())
 
         if self.hard:
-            memory_gate = (F.hardtanh((gate[:, :, None] - gate_hat) / self.resolution * 2 + 1) + 1) / 2
+            memory_gate = (F.hardtanh((g[:, :, None] - gate_hat) / self.resolution * 2 + 1) + 1) / 2
         else:
             memory_gate = F.sigmoid(
-                (gate[:, :, None] - gate_hat) / self.resolution * 10 + 5)  # bsz, ntimestep, nslots
+                (g[:, :, None] - gate_hat) / self.resolution * 10 + 5)  # bsz, ntimestep, nslots
         memory_gate = torch.cumprod(memory_gate, dim=2)  # bsz, ntimestep, nlookback+1
         memory_gate = torch.unbind(memory_gate, dim=1)
 
         if self.hard:
-            memory_gate_next = (F.hardtanh((gate_next[:, :, None] - gate_hat) / self.resolution * 2 + 1) + 1) / 2
+            memory_gate_next = (F.hardtanh((g_next[:, :, None] - gate_hat) / self.resolution * 2 + 1) + 1) / 2
         else:
             memory_gate_next = F.sigmoid(
-                (gate_next[:, :, None] - gate_hat) / self.resolution * 10 + 5)  # bsz, ntimestep, nslots
+                (gnext[:, :, None] - gate_hat) / self.resolution * 10 + 5)  # bsz, ntimestep, nslots
         memory_gate_next = torch.cumprod(memory_gate_next, dim=2)  # bsz, ntimestep, nlookback+1
         memory_gate_next = torch.unbind(memory_gate_next, dim=1)
 
-        return (memory_gate, memory_gate_next), gate, (emb_last[-self.nlookback:], cum_gate[:, -self.nslots:])
+        return (memory_gate, memory_gate_next), g, (emb_last[-self.nlookback:], cum_gate[:, -self.nslots:])
 
     def init_hidden(self, bsz):
         weight = next(self.parameters()).data
